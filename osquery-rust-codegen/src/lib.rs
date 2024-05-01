06ddfaf88d228962c949c355ff8cc8920fdcf226
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-01 23:03:16 +0200

    fix: allow usage of args proc macro from other repos (#1)
    Co-authored-by: tokcum <tobias.mucke@gmail.com>

--- osquery-rust-codegen/src/lib.rs
+++ osquery-rust-codegen/src/lib.rs
@@ -1,6 +1,5 @@
 use proc_macro::TokenStream;
 
-use std::fs;
 use std::str::FromStr;
 
 /// Defines a CLI for an osquery-rust based extension which is compliant to osquery interface.
@@ -18,9 +17,7 @@ use std::str::FromStr;
 #[proc_macro_attribute]
 pub fn args(_attr: TokenStream, input: TokenStream) -> TokenStream {
     //todo: error handling!
-    let s = fs::read_to_string("osquery-rust-codegen/src/cli.rs").unwrap();
-    let mut output: TokenStream = TokenStream::from_str(s.as_ref()).unwrap();
-
+    let mut output = TokenStream::from_str(include_str!("cli.rs")).unwrap();
     output.extend(input);
     output
 }
